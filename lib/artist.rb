e3dfcb80876a7d6b42ba26908498056da4e3bf68
Samara Grant <samara.amanda01@gmail.com>
2020-08-07 00:35:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,8 +14,8 @@ class Artist
     song.artist = self
   end
 
-  def add_song_by_name(name, genre)
-    song = self.new(name, genre)
+  def add_song_by_name(name)
+    song = Song.new(name)
     add_song(song)
   end
 
